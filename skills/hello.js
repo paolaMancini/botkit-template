4abb73b97b66ae8d6cfcda002635ac495c0958c2
paolaMancini <paola.mancini@italtel.com>
2018-02-09 14:10:35 +0100

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -3,6 +3,15 @@ module.exports = function(controller) {
     controller.hears([/^a-hello$/], 'direct_message,direct_mention', function(bot, message) {
 
         var welcome = `Hi <@personId:${event.actorId}>`;
+        
+        bot.say ({
+            text: welcome,
+            channel: event.channel
+        }, function (err, rawMessage) {
+            if (err) {
+                console.log("Error while postig back welcome message, err: " + err.message)
+                return;
+            }
 
         bot.startConversation(message, function(err, convo) {
 
