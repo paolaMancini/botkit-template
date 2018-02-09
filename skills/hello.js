a2a8177d6cad9b8d824a78ac730aec9be2185afd
paolaMancini <paola.mancini@italtel.com>
2018-02-09 16:23:12 +0100

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -1,20 +1,4 @@
-module.exports = function (controller) {
-
-    controller.hears([/^hello$/], 'direct_message,direct_mention', function (bot, message) {
-
-        bot.startConversation(message, function (err, convo) {
-            convo.say('This is a BotKit conversation sample.');
-
-            convo.ask('====> What is your favorite color?', function (response, convo) {
-                convo.say("Cool, I like '" + response.text + "' too!");
-                convo.next();
-            });
-        });
-
-    });
-};
-
-module.exports = function (controller) {
+ module.exports = function (controller) {
 
    controller.hears([/^hello$/], 'direct_message,direct_mention', function (bot, message) {
 
