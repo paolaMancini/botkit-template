9e5f017e8aaa20c99063b1f75ecacc3f3ccd8b9e
paolaMancini <paola.mancini@italtel.com>
2018-02-12 22:07:18 +0100

    Update question.js

--- skills/question.js
+++ skills/question.js
@@ -1,43 +1,47 @@
-controller.hears(['question me'], 'message_received', function(bot,message) {
+controller.hears(['/question me\b/'], 'message_received', function(bot,message) {
 
-  // start a conversation to handle this response.
-  bot.startConversation(message,function(err,convo) {
+ bot.createConversation(message, function(err, convo) {
 
-    convo.addQuestion('Shall we proceed Say YES, NO or DONE to quit.',[
-      {
-        pattern: 'done',
-        callback: function(response,convo) {
-          convo.say('OK you are done!');
-          convo.next();
-        }
-      },
-      {
-        pattern: bot.utterances.yes,
-        callback: function(response,convo) {
-          convo.say('Great! I will continue...');
-          // do something else...
-          convo.next();
+    // create a path for when a user says YES
+    convo.addMessage({
+            text: 'You said yes! How wonderful.',
+    },'yes_thread');
 
+    // create a path for when a user says NO
+    convo.addMessage({
+        text: 'You said no, that is too bad.',
+    },'no_thread');
+
+    // create a path where neither option was matched
+    // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
+    convo.addMessage({
+        text: 'Sorry I did not understand.',
+        action: 'default',
+    },'bad_response');
+
+    // Create a yes/no question in the default thread...
+    convo.addQuestion('Do you like cheese?', [
+        {
+            pattern: 'yes',
+            callback: function(response, convo) {
+                convo.gotoThread('yes_thread');
+            },
+        },
+        {
+            pattern: 'no',
+            callback: function(response, convo) {
+                convo.gotoThread('no_thread');
+            },
+        },
+        {
+            default: true,
+            callback: function(response, convo) {
+                convo.gotoThread('bad_response');
+            },
         }
-      },
-      {
-        pattern: bot.utterances.no,
-        callback: function(response,convo) {
-          convo.say('Perhaps later.');
-          // do something else...
-          convo.next();
-        }
-      },
-      {
-        default: true,
-        callback: function(response,convo) {
-          // just repeat the question
-          convo.repeat();
-          convo.next();
-        }
-      }
     ],{},'default');
 
-  })
+    convo.activate();
+});
 
 });
