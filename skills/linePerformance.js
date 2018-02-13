bdab10e67ba635d7b1e68a3e771e0b98b396a3fd
paolaMancini <paola.mancini@italtel.com>
2018-02-13 17:59:56 +0100

    Update linePerformance.js

--- skills/linePerformance.js
+++ skills/linePerformance.js
@@ -1,36 +1,18 @@
  
-module.exports = function (controller) {
-	   	var request = require('request');
-	        controller.hears( [/line performance\b/], 'direct_message,direct_mention', function(bot, message){
-		 
-		
-	           
-	            request('http://194.79.57.109:8080/SFapi/machines', function(error, response, body) {
-	                console.log('error:', error); // Print the error if one occurred
-	                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-	                console.log('body:', body); // Print the HTML for the Google homepage.
-	
-	                var jsonData = JSON.parse(body);
-			//var linea0 = jsonData.machines[0].machine;
-			//console.log('linea0:' ,linea0); 
-	                for (var i = 0; i < jsonData.machines.length; i++) {
-	                    var machine = jsonData.machines[i].machine;
-			    var alias = jsonData.machines[i].alias;
-			    var oee = jsonData.machines[i].alias;
-			 
-	                    console.log(machine);
-			    console.log(alias);
-			    console.log(oee);
-	               }
-	            });
-	
-	             
+var backendRestClient = require("./backendRestClient.js");
+var data=backendRestClient.jsonAllData;
+var machinesList=backendRestClient.machs;
+
+module.exports = function (controller) {  
+	 for (var i = 0; i < data.machines.length; i++) {
 		     
+		     var mpattern=mahcinesList.join("|");
+		     console.log('mpattern:',mpattern);
 		     bot.startConversation(message, function (err, convo) {
 
 		     convo.ask("Do you want more details?", 
 		     		[
-				{ pattern: "^yes|yep|y|Yes$",
+				{ pattern: mpattern,
 				    callback: function (response, convo) {
 					convo.gotoThread("yes");
 				    },
@@ -43,7 +25,7 @@ module.exports = function (controller) {
 				{
 				    default: true,
 				    callback: function (response, convo) {
-					convo.say("Sorry, I don't know this color. Try another one...");
+					convo.say("Sorry, I don't know this machine. Try another one...");
 					convo.repeat();
 					convo.next();
 				    }
