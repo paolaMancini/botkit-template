813d14dc5e3233189a1db1e76235c36226d2b0f2
paolaMancini <paola.mancini@italtel.com>
2018-02-13 18:02:52 +0100

    Update backendRestClient.js

--- skills/backendRestClient.js
+++ skills/backendRestClient.js
@@ -1,5 +1,5 @@
 var request = require('request');
- 
+var machs=[];
 
 var jsonAllData = allaMahcines = function(url) {
     request('http://194.79.57.109:8080/SFapi/machines', function(error, response, body) {
@@ -8,15 +8,18 @@ var jsonAllData = allaMahcines = function(url) {
         console.log('body:', body); // Print the HTML for the Google homepage.
 
         var jsonData = JSON.parse(body);
-        for (var i = 0; i < jsonData.length; i++) {
-            var counter = jsonData.machines[i];
-            console.log(counter.machine.oee);
-        }
+      for (var i = 0; i < jsonData.machines.length; i++) {
+         var machine = jsonData.machines[i].machine;
+         var oee = jsonData.machines[i].alias;
+         var alias = jsonData.machines[i].alias;
 
+         console.log(machine);
+         console.log(alias);
+         console.log(oee);
+         machs.push(machine);
         return jsonData;
     });
 }
 
-
-
 module.exports.jsonAllData;
+module.exports.machs;
