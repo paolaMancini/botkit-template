1af54b1edf946b0a766f6cf2e1325efebcdb68d6
paolaMancini <paola.mancini@italtel.com>
2018-02-13 17:22:59 +0100

    Update linePerformance.js

--- skills/linePerformance.js
+++ skills/linePerformance.js
@@ -12,7 +12,7 @@ module.exports = function (controller) {
 	                var jsonData = JSON.parse(body);
 			 
 			var linea0 = jsonData.machines[0].machine;
-			 console.log("linea0= "+linea0; 
+			 console.log(linea0); 
 	                //for (var i = 0; i < 7; i++) {
 	                    //var counter = jsonData.machines[i];
 	                    //console.log(jsonData.machines[i].machine);
