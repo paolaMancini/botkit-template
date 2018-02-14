ec94501d8bd8c57cccedf253d3be0e95b150e19c
paolaMancini <paola.mancini@italtel.com>
2018-02-15 00:14:18 +0100

    Update plant.js

--- skills/plant.js
+++ skills/plant.js
@@ -1,42 +1,33 @@
-
-
-
-//
-// Quiz: example of a muti-threaded conversation with timeout
-//
-//
 module.exports = function (controller) {
 
     controller.hears(['plant performance'], 'direct_message,direct_mention', function (bot, message) {
-
+      
         bot.startConversation(message, function (err, convo) {
+            convo.addMessage("The performances of the plant1 are: fakeMachine0 : 43.73%\n fakeMachine1: 31.56% \n fakeMachine2: 40.31%\n fakeMachine3: 43.65%\n fakeMachine4: 47.02%\ fakeMachine5: 38.52%");
+            convo.addQuestion('Do you want more details?', function (response, convo) {
+                convo.say('I love ' + response.text + ' too');
+                convo.next();
+            }, {}, 'linedetails');
 
-            // Default thread
-            convo.ask("Ready for a challenge (yes/no/cancel)", [
+            convo.ask("Which line do you want to watch over (no-line if you don't need details)", [
                 {
-                    pattern: "yes|yeh|sure|oui|si",
+                    pattern: "fakeMachine0,fakeMachine1,fakeMachine2,fakeMachine3,fakeMachine4,fakeMachine5,fakeMachine6",
                     callback: function (response, convo) {
-
-                        // Apply elaps time (in milliseconds) to next askQuestion
-                        convo.setTimeout(5000);
-                        convo.onTimeout(function (convo) {
-                            convo.gotoThread("missed");
-                        });
-
-                        convo.gotoThread('quiz');
+                        convo.say("You answred: '{{responses.linedetails}}'!");
+                        convo.next();
                     },
                 }
                 , {
                     pattern: "no|neh|non|na|birk",
                     callback: function (response, convo) {
-                        convo.say("Too bad, looking forward to play with you later...");
-                        convo.next();
+                        convo.gotoThread('linedetails');
                     },
                 }
                 , {
                     pattern: "cancel|stop|exit",
                     callback: function (response, convo) {
-                        convo.gotoThread('cancel');
+                        convo.say("Got it, cancelling...");
+                        convo.next();
                     },
                 }
                 , {
@@ -48,54 +39,6 @@ module.exports = function (controller) {
                     }
                 }
             ]);
-
-            // Cancel thread
-            convo.addMessage({
-                text: "Got it, cancelling...",
-                action: 'stop', // this marks the converation as unsuccessful
-            }, 'cancel');
-
-            // Quiz thread
-            convo.addMessage("Let's start", "quiz");
-            var challenge = pickChallenge();
-            convo.addQuestion("Question: " + challenge.question, [
-                {
-                    pattern: "^"+ challenge.answer + "$",
-                    callback: function (response, convo) {
-                        convo.gotoThread('success');
-                    },
-                }
-                , {
-                    pattern: "cancel|stop|exit",
-                    callback: function (response, convo) {
-                        convo.gotoThread('cancel');
-                    },
-                }
-                , {
-                    default: true,
-                    callback: function (response, convo) {
-                        convo.say("Sorry, wrong answer. Try again!");
-                        convo.repeat();
-                        convo.next();
-                    }
-                }
-            ], {}, 'quiz');
-
-            // Success thread
-            convo.addMessage("Congrats, you did it!", "success");
-
-            // Missed thread
-            convo.addMessage("Time elapsed! you missed it, sorry.", "missed");
         });
     });
 };
-
-
-function pickChallenge() {
-    var a = Math.round(Math.random()*5) + 4;
-    var b = Math.round(Math.random()*5) + 4;
-    return {
-        question : "" + a + " x " + b + " =",
-        answer : "" + (a * b)
-    }
-}
