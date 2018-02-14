4cb39b0cf474cb309e6f09c34516c40ae75fa47d
paolaMancini <paola.mancini@italtel.com>
2018-02-15 00:52:00 +0100

    Update linePerformance.js

--- skills/linePerformance.js
+++ skills/linePerformance.js
@@ -2,23 +2,36 @@ module.exports = function (controller) {
 	var request = require('request');
 	 controller.hears( [/line\b/], 'direct_message,direct_mention', function(bot, message){
  
-		 request('http://194.79.57.109:8080/SFapi/status?machine=', function(error, response, body) {
+module.exports = function (controller) {
+	var request = require('request');
+	 controller.hears( [/line performance\b/], 'direct_message,direct_mention', function(bot, message){
+	
+	
+		
+		 request('http://194.79.57.109:8080/SFapi/machines', function(error, response, body) {
 			 console.log('error:', error); // Print the error if one occurred
 			 console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 			 console.log('body:', body); // Print the HTML for the Google homepage.
 
-			 var jsonData = JSON.parse(body);
+		     var jsonData = JSON.parse(body);
+		   	 //var linea0 = jsonData.machines[0].machine;
+		     //console.log('linea0:' ,linea0); 
+			 for (var i = 0; i < jsonData.machines.length; i++) {
+				var machine = jsonData.machines[i].machine;
+			   	var alias = jsonData.machines[i].alias;
+			   	var oee = jsonData.machines[i].oee;
 		   
+				console.log(machine);
+			   	console.log(alias);
+			   	console.log(oee);
+			  }
 		 });
-
-		  
-		 
+  
 		 bot.startConversation(message, function (err, convo) {
- 
- 		 //var line=fakeMachine0|fakeMachine1|fakeMachine2|fakeMachine3|fakeMachine4|fakeMachine5
-		 convo.ask("which line do you eant to watch over?", 
+
+		 convo.ask("Do you want more details?", 
 				  [
-				 { pattern: "fakeMachine0|fakeMachine1|fakeMachine2|fakeMachine3|fakeMachine4|fakeMachine5",
+				 { pattern: "^yes|yep|y|Yes$",
 					 callback: function (response, convo) {
 						convo.gotoThread("yes");
 					 },
@@ -31,7 +44,7 @@ module.exports = function (controller) {
 				 {
 					 default: true,
 					 callback: function (response, convo) {
-						convo.say("Sorry, I don't know this color. Try another one...");
+						convo.say("Sorry, I don't know this line. Try another one...");
 						convo.repeat();
 						convo.next();
 					 }
@@ -39,7 +52,7 @@ module.exports = function (controller) {
 
 			   // yes thread
 			   convo.addMessage(
-	 			"The following performance values:fakeMachine0 : 43.73%'\n' fakeMachine1: 31.56% '\n' fakeMachine2: 40.31%'\n' fakeMachine3: 43.65%'\n' fakeMachine4: 47.02%'\n'fakeMachine5: 38.52%",
+				 "Cool, I love '{{responses.answer}}' too",
 				 "yes");
 				 
 			   convo.addMessage(
