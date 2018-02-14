4c731980e034b85584d4b999f58de20edf652929
paolaMancini <paola.mancini@italtel.com>
2018-02-14 23:37:51 +0100

    Update plant.js

--- skills/plant.js
+++ skills/plant.js
@@ -1,26 +1,17 @@
 module.exports = function (controller) {
-	var request = require('request');
-			controller.hears([/^plant performance/], "direct_message,direct_mention", function (bot, message) {
-				request('http://194.79.57.109:8080/SFapi/machines', function(error, response, body) {
-				console.log('error:', error); // Print the error if one occurred
-				console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-				console.log('body:', body); // Print the HTML for the Google homepage.
-				
-				var jsonData = JSON.parse(body);
-			});			  
 	
-			bot.startConversation(message, function (err, convo) {
+		  
 	
-				convo.ask("What is your favorite color?", [
-					{
-						pattern: "^blue|green|pink|red|yellow$",
-						callback: function (response, convo) {
-							convo.gotoThread("confirm_choice");
-						},
-					},
-					{
-						default: true,
-						callback: function (response, convo) {
+	 bot.startConversation(message, function (err, convo) {
+	 	convo.ask("What is your favorite color?", [{
+			pattern: "^blue|green|pink|red|yellow$",
+			callback: function (response, convo) {
+				convo.gotoThread("confirm_choice");
+				},
+			},
+			{
+				default: true,
+					callback: function (response, convo) {
 							convo.say("Sorry, I don't know this color. Try another one...");
 							convo.repeat();
 							convo.next();
