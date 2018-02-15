5a5449b9b96b2dc556a72cca16ee84c551912755
paolaMancini <paola.mancini@italtel.com>
2018-02-15 01:13:50 +0100

    Update help.js

--- skills/help.js
+++ skills/help.js
@@ -5,13 +5,15 @@ module.exports = function (controller) {
 
     controller.hears([/^help$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
-        text += "\n- " + bot.appendMention(message, "plant1 performance") + ": ask to pick a random color";
-        text += "\n- " + bot.appendMention(message, "line quality") + ": let a user pick a color among a set of options";
-        text += "\n- " + bot.appendMention(message, "line availability") + ": store picked color as a user preference";
-        text += "\n- " + bot.appendMention(message, "line OEE") + ": branch to another thread";
-        text += "\n- " + bot.appendMention(message, "line performance") + ": branch to another thread";
-        text += "\n- " + bot.appendMention(message, "variables") + ": enriched user-context among threads";
-        text += "\n\nI also understand:";
+        text += "\n- " + bot.appendMention(message, "plant OEEs") + ": Ask to know the OEE value about every line in the plant ";
+        text += "\n- " + bot.appendMention(message, "plant qualities") + ": Ask to know the OEE value about every line in the plant ";
+        text += "\n- " + bot.appendMention(message, "plant performances") + ": Ask to know the OEE value about every line in the plant ";
+        text += "\n- " + bot.appendMention(message, "line quality") + ": ask to know the quality value about a specific line";
+        text += "\n- " + bot.appendMention(message, "line availability") + ": ask to know the availability value about a specific line";
+        text += "\n- " + bot.appendMention(message, "line OEE") + ":  ask to know the OEE value about a specific line";
+        text += "\n- " + bot.appendMention(message, "line performance") + ": ask to know the performance value about a specific line";
+        text += "\n- " + bot.appendMention(message, "about") + ": ask to know who I am";
+    
         text += "\n- " + bot.appendMention(message, "help") + ": spreads the word about my skills";
         bot.reply(message, text);
     });
