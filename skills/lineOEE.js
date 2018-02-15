5eddf7b99fab40cc77acf62520ebf6779a4df213
paolaMancini <paola.mancini@italtel.com>
2018-02-15 01:29:22 +0100

    Update lineOEE.js

--- skills/lineOEE.js
+++ skills/lineOEE.js
@@ -1,7 +1,7 @@
 
  module.exports = function (controller) {
 
-    controller.hears(['which is the OEE value about the line fakeMachine0'], 'direct_message,direct_mention', function (bot, message) {
+    controller.hears([/^which is the OEE value about the line fakeMachine0$/'], 'direct_message,direct_mention', function (bot, message) {
 
         bot.startConversation(message, function (err, convo) {
 
