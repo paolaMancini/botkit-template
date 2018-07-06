a9d6fffbcb27bda46b080d25e8728ff09aeee0fa
paolaMancini <paola.mancini@italtel.com>
2018-07-06 12:36:35 +0200

    Update whoIam.js

--- skills/whoIam.js
+++ skills/whoIam.js
@@ -3,7 +3,7 @@ module.exports = function(controller) {
     controller.hears(['who are you| about you'], 'direct_message,direct_mention', function(bot, message) {
         
          if (this.identity) {
-           var mardown += `<br/>I am the **${this.identity.displayName}** bot, the Keys' Keeper. I can help you to get the link to click by your phone or your tablet in orden to open your JAGO door lock.";`;
+           var mardown += `<br/>I am the **${this.identity.displayName}** bot, the Keys' Keeper. I can help you to get the link to click by your phone or your tablet in orden to open your JAGO door lock.`;
         }else{
              var mardown = "This is Clavigero, the Keys' Keeper. I can help you to get the link to click by your phone or your tablet in orden to open your JAGO door lock.";
         }
