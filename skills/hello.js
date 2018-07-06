5743436c29831d46667017719cc99bf2509d9f8a
paolaMancini <paola.mancini@italtel.com>
2018-07-06 13:09:49 +0200

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -6,9 +6,9 @@ module.exports = function(controller) {
       console.log('message: ', message);
        console.log('message.data.personEmail: ', message.data.personEmail);   
        var message_options = [
-            "Hello @"+message.data.personEmail,
-            "Hi @"+message.data.personEmail,
-            "So glad to meet you again @"+message.data.personEmail
+            "Hello "+message.data.personEmail,
+            "Hi "+message.data.personEmail,
+            "So glad to meet you again "+message.data.personEmail
         ]
         var random_index = Math.floor(Math.random() * message_options.length)
         var chosen_message = message_options[random_index];
