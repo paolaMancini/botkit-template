a8a49e8cc2345209a1fd16d167277bd7f6e1eb2c
paolaMancini <paola.mancini@italtel.com>
2018-07-06 14:06:58 +0200

    Update JagoAPIsCalls.js

--- skills/JagoAPIsCalls.js
+++ skills/JagoAPIsCalls.js
@@ -2,36 +2,40 @@
 var debug = require("debug")("samples");
 var fine = require("debug")("samples:fine");
 
-module.exports.POSTuser = function(username,fname,uTagId, fromTime, toTime, cb) {
+module.exports.POSTuser = function(username, fname, uTagId, fromTime, toTime, cb) {
     var request = require("request");
-    
+
     var options = {
         method: 'POST',
         url: "https://api-cisco-otello-mi.jago.cloud/api/v1.1/users",
         "headers": {
             "accept": "application/json",
             "content-type": "application/json",
-            
-            "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiUk9MRV9NQU5BR0VSIiwidXNlcl9uYW1lIjoibWFuYWdlckBjaXNjby5jb20iLCJzY29wZSI6WyJvdGVsbG9fcmVhZCIsIm90ZWxsb193cml0ZSJdLCJ1c2VySWQiOjM2MjQsImF1dGhvcml0aWVzIjpbIlJPTEVfTUFOQUdFUiJdLCJqdGkiOiI1NTUxYzQ0Zi0yYmRmLTQyZGYtOTM4Zi01MmVjMTlhZDgyNTciLCJjbGllbnRfaWQiOiJhcHAifQ.P_hbCZrvmbGc9MKpOKU_XTbiaPrRIJ01R9ZwEcJrRQY",
-          }
-        };   
-    };
-    req.write(JSON.stringify({ email: 'u1',
-          firstname: 'u1',
-          lastname: 'u1',
-          password: 'ita123',
-          phone: 'string',
-          publicUser: true,
-          role: 'ROLE_GUEST',
-          userTagIds: [],
-          userTagIdsWithTime: 
-           [ { id: 3513,
-               interval: { from: 1530867587000, to: 1530889200000 } } ],
-          username: 'u1' }));
-        req.end();
-    }
 
+            "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiUk9MRV9NQU5BR0VSIiwidXNlcl9uYW1lIjoibWFuYWdlckBjaXNjby5jb20iLCJzY29wZSI6WyJvdGVsbG9fcmVhZCIsIm90ZWxsb193cml0ZSJdLCJ1c2VySWQiOjM2MjQsImF1dGhvcml0aWVzIjpbIlJPTEVfTUFOQUdFUiJdLCJqdGkiOiI1NTUxYzQ0Zi0yYmRmLTQyZGYtOTM4Zi01MmVjMTlhZDgyNTciLCJjbGllbnRfaWQiOiJhcHAifQ.P_hbCZrvmbGc9MKpOKU_XTbiaPrRIJ01R9ZwEcJrRQY",
+        }
+    };
+
+    req.write(JSON.stringify({
+        email: 'u1',
+        firstname: 'u1',
+        lastname: 'u1',
+        password: 'ita123',
+        phone: 'string',
+        publicUser: true,
+        role: 'ROLE_GUEST',
+        userTagIds: [],
+        userTagIdsWithTime: [{
+            id: 3513,
+            interval: { from: 1530867587000, to: 1530889200000 }
+        }],
+        username: 'u1'
+    }));
     
+    req.end();
+
+
+
     request(options, function(error, response, body) {
         if (error) {
             debug("1 could not retreive list of events, error: " + error);
@@ -41,29 +45,29 @@ module.exports.POSTuser = function(username,fname,uTagId, fromTime, toTime, cb) 
 
         if ((response < 200) || (response > 299)) {
             debug("1 could not retreive list of events, response: " + response);
-          
+
             return;
         }
 
         var userDetails = JSON.parse(body);
         //debug("fetched " + events.machines.length + " events");
         //fine(JSON.stringify(events));
-         
-        if (userDetail==null) {
+
+        if (userDetail == null) {
             cb(null, plants, "**User not created.**");
             return;
         }
-        if (userDetail.publicLink==null) {
+        if (userDetail.publicLink == null) {
             cb(null, plants, "**Link not found.**");
             return;
         }
-        
+
         var publicLink = userDetail.publicLink;
         var msg = "<br>";
-        msg +=publicLink;
-         
+        msg += publicLink;
+
         cb(null, plants, msg);
-      
+
     });
-}
- 
+
+};
