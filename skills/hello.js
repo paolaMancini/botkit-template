19abcbcf02c8117909f8e11e9771d143e7b206da
paolaMancini <paola.mancini@italtel.com>
2018-07-06 12:33:49 +0200

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -1,13 +1,15 @@
  
 module.exports = function(controller) {
 
-    controller.hears(['hello| hi'], 'direct_message,direct_mention', function(bot, message) {
-        var mardown = "This is Clavigero, the Keys' Keeper. I can help you to get the link to click by your phone or your tablet in orden to open your JAGO door lock.";
-        console.log('message: ', message);
+    controller.hears(['hello | hi'], 'direct_message,direct_mention', function(bot, message) {
+        
+       console.log('message: ', message);
        console.log('message.data.personEmail: ', message.data.personEmail);
          var message_options = [
-            "Hello",
-            "Hi" 
+            `Hello <@message.data.personEmail>!`,
+            `Hi <@message.data.personEmail>!`,
+            `So glad to meet you again  <@message.data.personEmail>!`
+             
         ]
 
         var random_index = Math.floor(Math.random() * message_options.length)
