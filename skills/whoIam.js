838790370719814ad91890e06cd4547844485385
paolaMancini <paola.mancini@italtel.com>
2018-07-06 12:04:40 +0200

    Update whoIam.js

--- skills/whoIam.js
+++ skills/whoIam.js
@@ -1,7 +1,7 @@
 module.exports = function(controller) {
 
     controller.hears(['who are you| about you'], 'direct_message,direct_mention', function(bot, message) {
-        var mardown = "This is Clavigero, the Keys' Keeper. I can help you to get the link to click by you phone or your tablet in orden to open your JAGO door lock.";
+        var mardown = "This is Clavigero, the Keys' Keeper. I can help you to get the link to click by your phone or your tablet in orden to open your JAGO door lock.";
         mardown += bot.appendMention(message, "help");
 
         bot.reply(message, mardown);
