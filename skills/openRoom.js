088cd5b0b90ccf3077115560a0f3b0327e41b9cc
paolaMancini <paola.mancini@italtel.com>
2018-07-06 13:59:07 +0200

    Update openRoom.js

--- skills/openRoom.js
+++ skills/openRoom.js
@@ -1,6 +1,22 @@
 
  
  module.exports = function(controller) {
-  POSTuser = function(username,fname,uTagId, fromTime, toTime, cb) {}
-     
+   controller.hears([/'open|access'/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    POSTuser = function(username, fname, uTagId, fromTime, toTime, cb) {
+        if (err) {
+            bot.reply(message, "Something went wrong during connection with JAGO system");
+            return;
+        }
+
+        if (publickLink == null) {
+            bot.reply(message, "Access not authorized");
+            return;
+        }
+
+        console.log("publickLink: ", publickLink);
+        text += "You are able to open the door by click on the following link from Otello App <br>";
+
+        bot.reply(message, text);
+    }
+  }
 }
