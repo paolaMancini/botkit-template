ce0fbda793954cf016ce2656e37aed21d57e4e20
paolaMancini <paola.mancini@italtel.com>
2018-07-06 12:07:13 +0200

    Update greetings.js

--- skills/greetings.js
+++ skills/greetings.js
@@ -1,16 +1,17 @@
+ 
 module.exports = function(controller) {
 
-    controller.hears([/'how'/i], ['direct_message', 'direct_mention', 'mention'], function(bot, message) {
+    controller.hears(['how are you| feeling'], 'direct_message,direct_mention', function(bot, message) {
+        var mardown = "This is Clavigero, the Keys' Keeper. I can help you to get the link to click by your phone or your tablet in orden to open your JAGO door lock.";
         var message_options = [
             "Fine thanks!",
             "Very well, thank you.",
             "Not so bad, thanks for asking.",
             "Very good, thank you."
         ]
-        
         var random_index = Math.floor(Math.random() * message_options.length)
         var chosen_message = message_options[random_index];
-       
+ 
         bot.reply(message, chosen_message);
-     });
+    });
 }
