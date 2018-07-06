5578b13389b3086fa9ca98f573eff561e980b9d5
paolaMancini <paola.mancini@italtel.com>
2018-07-06 11:22:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,14 +56,19 @@ var controller = Botkit.sparkbot({
     secret: process.env.SECRET, // this is a RECOMMENDED security setting that checks of incoming payloads originate from Cisco Spark
     webhook_name: process.env.WEBHOOK_NAME || ('built with BotKit (' + env + ')')
 });
-var options = {
-    dialogflow_token: process.env.dialogflow
-};
-
-var dialogflowMiddleware = require('botkit-middleware-dialogflow')(options);
+var dialogflowMiddleware = require('botkit-middleware-dialogflow')({
+  token: process.env.dialogflow
+});
 
 controller.middleware.receive.use(dialogflowMiddleware.receive);
-//bot.startRTM();
+
+controller.hears('test', 'message_received', dialogflowMiddleware.hears, function(
+    bot,
+    message
+) {
+  console.log('MESSAGE:', message);
+    bot.reply(message, 'Hello!');
+});
 
 var bot = controller.spawn({
 }); 
