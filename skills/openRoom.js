69dc088b89ea9c37d435b9ddfcf653be46666a44
paolaMancini <paola.mancini@italtel.com>
2018-07-09 11:35:27 +0200

    Update openRoom.js

--- skills/openRoom.js
+++ skills/openRoom.js
@@ -1,51 +1,35 @@
-var JagoCalls = require ("./JagoAPIsCalls");
+var JagoCalls = require("./JagoAPIsCalls");
 
 module.exports = function(controller) {
 
     controller.hears(['access|open|token'], 'direct_message,direct_mention', function(bot, message) {
         console.log('message: ', message);
-         
+
         var request = require("request");
         var random_index = Math.floor(Math.random() * (1000 - 1) + 1);
-        var user="u"+random_index;
-//1531134000000 1531141200000
-        var options = {
-            method: "POST",
-            url: "https://api-cisco-otello-mi.jago.cloud/api/v1.1/users",
-            headers: {
-                 
-               "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiUk9MRV9NQU5BR0VSIiwidXNlcl9uYW1lIjoibWFuYWdlckBjaXNjby5jb20iLCJzY29wZSI6WyJvdGVsbG9fcmVhZCIsIm90ZWxsb193cml0ZSJdLCJ1c2VySWQiOjM2MjQsImF1dGhvcml0aWVzIjpbIlJPTEVfTUFOQUdFUiJdLCJqdGkiOiI1NTUxYzQ0Zi0yYmRmLTQyZGYtOTM4Zi01MmVjMTlhZDgyNTciLCJjbGllbnRfaWQiOiJhcHAifQ.P_hbCZrvmbGc9MKpOKU_XTbiaPrRIJ01R9ZwEcJrRQY",
-                "content-type": "application/json",
-                "accept": "application/json"
-            },
-            body: {
-                "email": user,
-                "firstname": user,
-                "lastname": user,
-                "password": "ita123",
-                "phone": "string",
-                "publicUser": true,
-                "role": "ROLE_GUEST",
-                "userTagIds": [],
-                "userTagIdsWithTime": [{
-                    "id": 3513,
-                    "interval": { "from": 1530889200000, "to": 1530903600000 }
-                }],
-                "username": user
-            },
-            json: true
-        };
+        var user = "u" + random_index;
 
-        request(options, function(error, response, body) {
-            if (error) throw new Error(error);
+        JagoAPIsCalls.POSTUser(user, user, 3513, 1531134000000, 1531141200000, function(err, data, text) {
+            if (err) {
+                bot.reply(message, "Jago system not reached! err: ", err);
+                return;
+            }
 
-            console.log('body: ',body);
-            console.log('###################');
-             
-            var publicLink=body.publicLink;
-            bot.reply(message, "click on "+publicLink+"  from your Otello App to access the room");
+            if (data.length == 0) {
+                bot.reply(message, "Request failed!");
+                return;
+            }
+
+            publcLink = data.publicLink;
+            console.log("publcLink: ", publcLink);
+            console.log("publcLink: ", publcLink);
+
+            bot.reply(message, "click on " + publicLink + "  from your Otello App to access the room");
+
+
+
+
         });
-        
-       
+
     });
 }
