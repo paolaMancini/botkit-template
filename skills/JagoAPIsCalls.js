c1742b40adc7abddd5392a48d4af6cb3d7f62bb8
paolaMancini <paola.mancini@italtel.com>
2018-07-09 10:59:21 +0200

    Update JagoAPIsCalls.js

--- skills/JagoAPIsCalls.js
+++ skills/JagoAPIsCalls.js
@@ -5,37 +5,43 @@ var fine = require("debug")("samples:fine");
 module.exports.POSTuser = function(username, fname, uTagId, fromTime, toTime, cb) {
     var request = require("request");
 
-    var options = {
-        method: 'POST',
-        url: "https://api-cisco-otello-mi.jago.cloud/api/v1.1/users",
-        "headers": {
-            "accept": "application/json",
-            "content-type": "application/json",
+   var options = {
+            method: "POST",
+            url: "https://api-cisco-otello-mi.jago.cloud/api/v1.1/users",
+            headers: {
+                 
+               "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiUk9MRV9NQU5BR0VSIiwidXNlcl9uYW1lIjoibWFuYWdlckBjaXNjby5jb20iLCJzY29wZSI6WyJvdGVsbG9fcmVhZCIsIm90ZWxsb193cml0ZSJdLCJ1c2VySWQiOjM2MjQsImF1dGhvcml0aWVzIjpbIlJPTEVfTUFOQUdFUiJdLCJqdGkiOiI1NTUxYzQ0Zi0yYmRmLTQyZGYtOTM4Zi01MmVjMTlhZDgyNTciLCJjbGllbnRfaWQiOiJhcHAifQ.P_hbCZrvmbGc9MKpOKU_XTbiaPrRIJ01R9ZwEcJrRQY",
+                "content-type": "application/json",
+                "accept": "application/json"
+            },
+            body: {
+                "email": username,
+                "firstname": "buddy",
+                "lastname": "buddy",
+                "password": "ita123",
+                "phone": "string",
+                "publicUser": true,
+                "role": "ROLE_GUEST",
+                "userTagIds": [],
+                "userTagIdsWithTime": [{
+                    "id": uTagId,
+                    "interval": { "from": fromTime, "to": toTime }
+                }],
+                "username": username
+            },
+            json: true
+        };
 
-            "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiUk9MRV9NQU5BR0VSIiwidXNlcl9uYW1lIjoibWFuYWdlckBjaXNjby5jb20iLCJzY29wZSI6WyJvdGVsbG9fcmVhZCIsIm90ZWxsb193cml0ZSJdLCJ1c2VySWQiOjM2MjQsImF1dGhvcml0aWVzIjpbIlJPTEVfTUFOQUdFUiJdLCJqdGkiOiI1NTUxYzQ0Zi0yYmRmLTQyZGYtOTM4Zi01MmVjMTlhZDgyNTciLCJjbGllbnRfaWQiOiJhcHAifQ.P_hbCZrvmbGc9MKpOKU_XTbiaPrRIJ01R9ZwEcJrRQY",
-        }
-    };
+        request(options, function(error, response, body) {
+            if (error) throw new Error(error);
 
-    req.write(JSON.stringify({
-        email: 'u1',
-        firstname: 'u1',
-        lastname: 'u1',
-        password: 'ita123',
-        phone: 'string',
-        publicUser: true,
-        role: 'ROLE_GUEST',
-        userTagIds: [],
-        userTagIdsWithTime: [{
-            id: '3513',
-            interval: { from:'1530867587000', to: '1530889200000' }
-        }],
-        username: 'u1'
+            console.log('body: ',body);
+            console.log('###################');
+             
+            var publicLink=body.publicLink;
     }));
     
-    req.end();
-
-
-
+  
     request(options, function(error, response, body) {
         if (error) {
             debug("1 could not retreive list of events, error: " + error);
