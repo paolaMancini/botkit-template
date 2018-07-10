2602294f8bcf1b61c1bdc8750b2231551c1aa832
paolaMancini <paola.mancini@italtel.com>
2018-07-10 10:12:24 +0200

    Update rooms.js

--- skills/rooms.js
+++ skills/rooms.js
@@ -5,10 +5,7 @@ module.exports = function(controller) {
 
     controller.hears(['room|lock'], 'direct_message,direct_mention', function(bot, message) {
         console.log('message: ', message);
-
-      
-      
-       
+ 
         
         //JagoCalls.POSTuser(user, user, 3513, 1531144800000, 1531159200000, function(err, data, text) {
         JagoCalls.GETsmartLocks(function(err, data, text) {
@@ -21,16 +18,10 @@ module.exports = function(controller) {
                 bot.reply(message, "Request failed!");
                 return;
             }
+ 
 
-            publicLink = data.publicLink;
-            console.log("publcLink: ", publicLink);
-        
-
-            bot.reply(message, "click on " + publicLink + "  from your Otello App to access the room");
-
-
-
-
+            bot.reply(message, text);
+ 
         });
 
     });
