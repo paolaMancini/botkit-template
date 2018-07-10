dc46ff44c809d5c51dc5f70c2dda121adf13f433
paolaMancini <paola.mancini@italtel.com>
2018-07-10 15:34:56 +0200

    Update openRoom.js

--- skills/openRoom.js
+++ skills/openRoom.js
@@ -4,19 +4,30 @@ module.exports = function(controller) {
 
     controller.hears(['access|open|token'], 'direct_message,direct_mention', function(bot, message) {
         console.log('message: ', message);
+        
+        console.log('message: ', message);
+        var roomName = message.match[1];
+
+        console.log("roomName  received: ", roomName );
 
         var request = require("request");
+        
+        // Create random number for dummy user
         var random_index = Math.floor(Math.random() * (1000 - 1) + 1);
         var user = "u" + random_index;
-
-      
+        
+        //Manage time range for access duration
         var today = new Date();
         console.log(today);
         var tomorrow = new Date();
         tomorrow.setDate(today.getDate()+1);
         console.log(tomorrow);
+          
+        JagoCalls.GETIdSmartLockByName(roomName, function(err, data, text)){
+             var id=null;  
+                                     
+        }
         
-        //JagoCalls.POSTuser(user, user, 3513, 1531144800000, 1531159200000, function(err, data, text) {
         JagoCalls.POSTuser(user, user, 3513, today.getTime(), tomorrow.getTime(), function(err, data, text) {
             if (err) {
                 bot.reply(message, "Jago system not reached! err: ", err);
