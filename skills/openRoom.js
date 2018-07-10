b39c13adb5c4d967efc96c97ad1ac31e6164c8af
paolaMancini <paola.mancini@italtel.com>
2018-07-10 17:05:05 +0200

    Update openRoom.js

--- skills/openRoom.js
+++ skills/openRoom.js
@@ -2,7 +2,7 @@ var JagoCalls = require("./JagoAPIsCalls");
 
 module.exports = function(controller) {
 
-    controller.hears(['access |open'], 'direct_message,direct_mention', function(bot, message) {
+    controller.hears(['access |authorize'], 'direct_message,direct_mention', function(bot, message) {
         console.log('message: ', message);
 
         //var roomName = message.match[0];
