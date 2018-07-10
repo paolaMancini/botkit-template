207d039d2b8e9d31f7c71125196a384a759f9fd9
paolaMancini <paola.mancini@italtel.com>
2018-07-11 00:23:53 +0200

    Update help.js

--- skills/help.js
+++ skills/help.js
@@ -5,9 +5,9 @@ module.exports = function (controller) {
 
     controller.hears([/^help$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
-        text += "\n- " + bot.appendMention(message, "open <room>") + ": let a user to receive the url to click opening the room ";
-        text += "\n- " + bot.appendMention(message, "rooms") + ": let a user pick a color among a set of options";
-        text += "\n- " + bot.appendMention(message, "room detail") + ": store picked color as a user preference";
+        text += "\n- " + bot.appendMention(message, "open <Digitaliani|Office 301>") + ": let a user to receive the url to click opening the room ";
+        text += "\n- " + bot.appendMention(message, "locks") + ": let a user to receive datails about lcoks";
+         
         bot.reply(message, text);
     });
 }
