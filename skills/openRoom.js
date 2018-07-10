7ee4cfb2c1995fc897f05f49c1e2c0b832ad53c3
paolaMancini <paola.mancini@italtel.com>
2018-07-10 19:17:16 +0200

    Update openRoom.js

--- skills/openRoom.js
+++ skills/openRoom.js
@@ -11,7 +11,8 @@ module.exports = function(controller) {
             //var roomName = "digitaliani";
             var roomName = message.match[1];
             console.log("roomName.toLowerCase(): ",roomName.toLowerCase());
-         console.log(""Office 301".toLowerCase(): ","Office 301".toLowerCase());
+            console.log("Office 301".toLowerCase(): ","Office 301".toLowerCase());
+        
             if ((roomName != "digitaliani") && (roomName.toLowerCase() != "Office 301".toLowerCase())) {
                 bot.reply(message, "Room " + roomName + "not available");
             } else {
