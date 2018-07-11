5bd182d4e0843f83ca5c55be5088c9e88e3914df
paolaMancini <paola.mancini@italtel.com>
2018-07-11 16:20:13 +0200

    Update z-fallback.js

--- skills/z-fallback.js
+++ skills/z-fallback.js
@@ -3,10 +3,19 @@
 //
 module.exports = function (controller) {
 
-    controller.hears([".*"], 'direct_message,direct_mention', function (bot, message) {
-        var mardown = "Sorry, I did not understand.<br/>Try "
-            + bot.appendMention(message, "help");
-            
-        bot.reply(message, mardown);
+     
+    
+     controller.hears([".*"], 'direct_message,direct_mention', function (bot, message) {
+      
+        var message_options = [
+            "Sorry, I did not understand.<br/>",
+            "Sorry! It's not clear for me.",
+            "What do you mean?",
+            "I didn't get it. Please, try again ..."
+        ];
+        var random_index = Math.floor(Math.random() * message_options.length);
+        var chosen_message = message_options[random_index];
+ 
+        bot.reply(message, chosen_message);
     });
 }
