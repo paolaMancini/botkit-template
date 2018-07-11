92e002b0b395c345fab93cbd9dbcffa5eefaee28
paolaMancini <paola.mancini@italtel.com>
2018-07-11 15:02:32 +0200

    Update greetings.js

--- skills/greetings.js
+++ skills/greetings.js
@@ -2,7 +2,7 @@
 module.exports = function(controller) {
 
     controller.hears(['how are you| feeling'], 'direct_message,direct_mention', function(bot, message) {
-        var mardown = "This is Clavigero, the Keys' Keeper. I can help you to get the link to click by your phone or your tablet in orden to open your JAGO door lock.";
+       oor lock.";
         var message_options = [
             "Fine thanks!",
             "Very well, thank you.",
