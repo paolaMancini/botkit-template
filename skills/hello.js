bc028a8d913ecc8f5c9447ce8b8d7510a391896e
paolaMancini <paola.mancini@italtel.com>
2018-07-12 15:11:09 +0200

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -2,7 +2,7 @@
  
 module.exports = function(controller) {
 
-    controller.hears(['hello'/i], 'direct_message,direct_mention', function(bot, message) {
+    controller.hears([/hello/i] , 'direct_message,direct_mention', function(bot, message) {
       console.log('message: ', message);
        console.log('message.data.personEmail: ', message.data.personEmail);   
        var message_options = [
