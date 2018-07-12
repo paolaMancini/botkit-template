6fe202f73472facb6af825c0d8ab0def6abe0b59
paolaMancini <paola.mancini@italtel.com>
2018-07-12 12:05:51 +0200

    Update help.js

--- skills/help.js
+++ skills/help.js
@@ -5,8 +5,8 @@ module.exports = function (controller) {
 
     controller.hears([/^help$/], 'direct_message,direct_mention', function (bot, message) {
         var text = "Here are my skills:";
-        text += "\n- " + bot.appendMention(message, "open <Digitaliani|Office 301>") + ": let a user to receive the url to click opening the room ";
-        text += "\n- " + bot.appendMention(message, "locks") + ": let a user to receive datails about lcoks";
+        text += "\n- " + bot.appendMention(message, "open <**Digitaliani** |**Office 301**>") + ": let a user to receive the url granting access to the smart lock;"
+        text += "\n- " + bot.appendMention(message, "locks") + ": let a user to receive datails about smart locks";
          
         bot.reply(message, text);
     });
