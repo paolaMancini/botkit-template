b9ebcf8efe2490a02456828fb1cd25fb5741fc27
paolaMancini <paola.mancini@italtel.com>
2018-07-13 11:38:58 +0200

    Update a-hello.js

--- skills/a-hello.js
+++ skills/a-hello.js
@@ -1,13 +1,25 @@
-var dialogflowMiddleware = require('botkit-middleware-dialogflow')({
-  token: process.env.dialogflow
-});
-
-controller.middleware.receive.use(dialogflowMiddleware.receive);
-
-controller.hears('test', 'message_received', dialogflowMiddleware.hears, function(
+module.exports = function(controller) {
+   controller.hears('Default Welcome Intent', 'message_received', dialogflowMiddleware.hears, function(
     bot,
     message
 ) {
   console.log('MESSAGE:', message);
     bot.reply(message, 'Hello!');
 });
+      console.log('message: ', message);
+       console.log('message.data.personEmail: ', message.data.personEmail);   
+       var message_options = [
+            "Hello "+message.data.personEmail,
+            "Hi "+message.data.personEmail,
+            "So glad to meet you again "+message.data.personEmail
+        ]
+        var random_index = Math.floor(Math.random() * message_options.length)
+        var chosen_message = message_options[random_index];
+ 
+        bot.reply(message, chosen_message);
+    });
+}
+
+ 
+
+
