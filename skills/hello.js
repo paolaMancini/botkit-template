d621def422674dec4aefeae0730d29f48f61e156
paolaMancini <paola.mancini@italtel.com>
2018-07-13 10:42:26 +0200

    Update hello.js

--- skills/hello.js
+++ skills/hello.js
@@ -1,28 +1,7 @@
  
  
 module.exports = function(controller) {
-     
-
-    controller.hears([/hello/i] , 'direct_message,direct_mention', function(bot, message) {
-     if (controller.config.limit_to_domain) {
-         var domains = [];
-         if (typeof(controller.config.limit_to_domain) == 'string') {
-             domains = [controller.config.limit_to_domain];
-         } else {
-             domains = controller.config.limit_to_domain;
-         }
-         var allowed = false;
-         var userEmail = message.data.personEmail;
-         for (var d = 0; d < domains.length; d++) {
-             if (userEmail.includes(domains[d]) || userEmail.includes('@sparkbot.io')) {
-                 allowed = true;
-             }
-         }
-         if (!allowed) {
-             console.log('*** WARNING *** : this message came from ' + userEmail + ' a domain that is outside of the allowed list', controller.config.limit_to_domain);
-             // this message came from a domain that is outside of the allowed list.
-             return false;
-         }
+      
      }
       console.log('message: ', message);
        console.log('message.data.personEmail: ', message.data.personEmail);   
